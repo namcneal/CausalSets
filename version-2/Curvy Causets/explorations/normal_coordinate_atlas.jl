7c8b12d1b37bbf4efec4d1f41fd554272a959c83
Noah <namcneal@umich.edu>
2022-07-22 15:25:36 -0400

    Tried another approach to generating a random Riemannian field. I'm going to go back to the Fourier stuff

--- version-2/Curvy Causets/explorations/normal_coordinate_atlas.jl
+++ version-2/Curvy Causets/explorations/normal_coordinate_atlas.jl
@@ -2,73 +2,57 @@ using Flux
 using LinearAlgebra
 
 include("../lib/plotting_utils.jl")
+include("../lib/normal_coordinate_utils.jl")
 
 ##
 
 struct CommonParams
     dim::Int64
-    max_frequency::Int64
-    network_inputs::Vector{Vector{Int64}}
-    network_activation::Function
+    possible_indices::Vector{Vector{Int}}
+    onehot_possible_indices::Flux.OneHotMatrix
 end
 
-struct FourierData
+struct Riemann 
     params::CommonParams
     network::Flux.Chain
-    shifts::Vector{Float32}
 end
 
-function CommonParams(dim::Int64, max_frequency::Int64, activation::Function=tanh)
-    frequencies = -max_frequency:max_frequency
-    
-    iterator_arg   = repeat([frequencies], dim)
-    inputs = collect(Iterators.product(iterator_arg...))
-    inputs = map(tup -> [x for x in tup], inputs) |> vec
+function CommonParams(dim::Int64)
+    index_range= 1:dim
+    iterator_arg   = repeat([index_range], dim)
+    possible_indices = Iterators.product(iterator_arg...) |> collect |> vec
 
+    possible_indices
+    onehots = Flux.onehotbatch(possible_indices, possible_indices)
 
-    return CommonParams(dim, max_frequency, inputs, activation)
+    return CommonParams(dim, possible_indices, onehots)
 end
 
-function FourierData(params::CommonParams, network_layer_structure::Vector{Int64}=Int64[],
-                     maximum_shift::Float64=20.0)
-    layer_sizes = [params.dim, network_layer_structure..., 1]
+function Riemann(params::CommonParams,
+                 network_layer_structure::Vector{Int64}=Int64[],
+                 network_activation::Function=NNlib.mish)
+
+    layer_sizes = [dim^4 + dim, network_layer_structure..., 1]
 
     layers = []
     for i in 1:(length(layer_sizes) - 1)
-        layer = Dense(layer_sizes[i] => layer_sizes[i+1], params.network_activation)
+        layer = Dense(layer_sizes[i] => layer_sizes[i+1], network_activation)
         push!(layers, layer)
     end
 
-    return FourierData(params, Chain(layers...), maximum_shift*rand(Float32, length(params.network_inputs)))
+    return Riemann(params, Chain(layers...))
+
 end
 
-function (fourier::FourierData)(x::Vector{Float64})
-    output = [convert(Float32, 0.0)]
-    x = convert.(Float32, x)
+# function indices2network_input()
+# end
 
-    for (i,input) in enumerate(fourier.params.network_inputs)
-        coefficient = fourier.network(vec(input))[1]
-        θ = dot(x, input)
-        output .+= coefficient * sin(θ - fourier.shifts[i]) / (norm(input) + 1)
-    end
-    
-    return output
-end
+# function (riemann::Riemann)(a::Int64, b::Int64, c::Int64, d::Int64)
+#     if (a==b) || (c==d)
+#         return 0.0
+#     end
 
-dim = 2
-max_frequency = 5
-params = CommonParams(dim, max_frequency, tanh)
-fourier = FourierData(params, [1])
+# end
 
-
-##
-
-num_points = 2000
-sidelength = 10.0
-points = 2*sidelength*rand(Float64, (dim, num_points)) .- sidelength
-points = [points[:, i] for i in 1:num_points]
-@time field_values = [fourier(point)[1] for point in points]
-
-limits = [-sidelength, sidelength, -sidelength, sidelength]
-@time f, ax = plot_field(points, convert.(Float64, field_values), limits)
-f
+CommonParams(2)
+# Riemann(4)
--- version-2/Curvy Causets/lib/normal_coordinate_utils.jl
+++ version-2/Curvy Causets/lib/normal_coordinate_utils.jl
@@ -5,31 +5,6 @@ using LinearAlgebra
 
 ##
 
-function first_indexpair_asymmetric(riemann::Array)
-    dim = size(riemann)[1]
-
-    should_be_zero = 0.0
-
-    for a in 1:dim, b in 1:dim, c in 1:dim, d in 1:dim
-        @inbounds should_be_zero += (riemann[a,b,c,d] + riemann[b,a,c,d])^2
-    end
-
-    return should_be_zero
-end
-
-function last_indexpair_asymmetric(riemann::Array)
-    dim = size(riemann)[1]
-
-    should_be_zero = 0.0
-
-    # For any choice of indices in the first two coordinates, the resulting d x d tensor
-    # should be antisymmetric
-    for a in 1:dim, b in 1:dim, c in 1:dim, d in 1:dim
-        @inbounds should_be_zero += (riemann[a,b,c,d] .+ riemann[a,b,d,c])^2
-    end
-
-    return should_be_zero  
-end
 
 function check_algebraic_bianchi(riemann::Array)
     dim = size(riemann)[1]
@@ -55,65 +30,8 @@ function check_interchange_of_pairs(riemann::Array)
     return should_be_zero 
 end
 
-function loss(riemann::Array)
-    checks = [first_indexpair_asymmetric, 
-              last_indexpair_asymmetric, 
-              check_algebraic_bianchi]
-
-
-    errs = [check(riemann) for check in checks]
-    return norm(errs)
-end
-
 ##
 
-function random_riemann(dim::Int64, eval_at::Float64; 
-                        tol::Float64=1e-10, max_iters::Float64=Inf, return_training_stats::Bool=false)
-    
-    eval_at = convert(Float32, eval_at)
-    network = Chain(Dense(1 => dim^4), arr -> reshape(arr, (dim,dim,dim,dim)))
-
-    local training_loss
-    training_losses = [Inf]
-    test_losses = Float64[]
-    
-    params = Flux.params(network)
-    opt   = ADAM()
-
-    iter_count = 0
-    while training_losses[end] > tol && iter_count < max_iters
-        gradients = gradient(params) do 
-            training_loss = loss(network([eval_at]))
-
-            return training_loss
-        end
-
-        push!(training_losses, training_loss)
-        push!(test_losses, check_interchange_of_pairs(network([eval_at])))
-        Flux.update!(opt, params, gradients)
-    end
-
-    riemann = convert.(Float64, network([eval_at]))
-
-    if return_training_stats
-        return riemann, training_losses[2:end], test_losses
-    else
-        return riemann
-    end
-
-
-end
-
-# dim = 2
-# @time riemann, train, test = random_riemann(4, 100.0; max_iters=1e6)
-
-# f  = Figure()
-# ax = Axis(f[1,1])
-# plot!(ax, log.(10, train), color=:red)
-# plot!(ax, log.(10, test),  color=:blue)
-# f
-
-##
 
 function flat_metric(dim::Int64)
     η = Matrix(1.0I, (dim, dim))
@@ -155,58 +73,3 @@ end
 function normal_ricci()
 end
 
-# dim = 4
-# riemann = random_riemann(dim, 1.0)
-
-# test_point = ones(dim)
-# metric  = normal_metric(riemann,  test_point)
-# tetrad  = normal_tetrad(riemann, metric, test_point)
-
-# η = flat_metric(4)
-# @einsum test_metric[a,b] := tetrad[a,s] * η[s,t] * tetrad[t,b]
-
-##
-
-function make_causet(event_coordinates::Matrix{Float64}, tetrad::Function, metric::Function)
-    num_events = size(event_coordinates)[2]
-
-    # Initiate an empty directed graph's adjacency_matrix
-    adjacency  = LightGraphs.adjacency_matrix(SimpleDiGraph(num_events))
-    
-    # Check each causal relation
-    index_pairs = [(i,j) for i in 1:num_events for j in 1:num_events]
-    
-    lk = ReentrantLock()
-    Threads.@threads for (i,j) in index_pairs
-        # Don't bother going through the computation if we have already
-        # determined the events are causally connected 
-        if adjacency[i,j] != 1
-
-            # We are considering the vector going from i to j
-            vector_i2j  = event_coordinates[:, j] - event_coordinates[:, i]
-            interval    = vector_i2j' * metric(event_coordinates[:, i]) * vector_i2j
-            
-            # If the separation vector is timelike and future-orientated at xi 
-            if interval <= 0 && tetrad(event_coordinates[:,i])[1,:]' * vector_i2j < 0
-                begin 
-                    lock(lk)
-
-                    try
-                        # Add the connection from xi to xj
-                        adjacency[i, j] = 1
-
-                        # Then add connections from every event in the past of xi, 
-                        # its inneighbors, to xj
-                        adjacency[adjacency[:,i] .> 0, j] .= 1
-
-                    finally
-                        unlock(lk)
-                    end
-                end
-            end
-        end
-
-    end 
-            
-    return event_coordinates, SimpleDiGraph(adjacency)
-end
